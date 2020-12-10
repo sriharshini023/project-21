4853146dd074fe5ac2e7f842d1af0c5d90493308
sriharshini023 <75304890+sriharshini023@users.noreply.github.com>
2020-12-10 09:45:55 +0530

    Add files via upload

---
+++ finaly.js
@@ -0,0 +1,46 @@
+var wall,thickness
+var bullet,speed,weight;
+
+
+
+
+function setup() {
+  createCanvas(1600,400);
+bullet=createSprite(50,200,50,50)
+wall=createSprite(1200,200,thickness,height/2);
+wall.shapeColour=colour(80,80,80)
+  thickness=random(22,83)
+  speed=random(223,321)
+  weight=random(30,52)
+ 
+  
+}
+
+function draw() {
+  function hasCollided(lbullet,lwall)
+{
+bulletRightEdge=lbullet.x+lbullet.width;
+wallLeftEdge=lwall.x;
+{
+  return true
+}
+return false
+}
+  drawSprites();
+}
+
+if(hasCollided(bullet,wall))
+{
+bullet.velocityX=0
+var damage=0.5*weight*speed*speed/(thickness*thickness*thickness)
+
+
+if(damage>10)
+{
+  wall.shapeColour=colour(255,0,0)
+}
+if(damage<10)
+{
+  wall.shapeColour=colour(0,255,0)
+}
+}
